54a1ed8137b4a0f3223d7b18b4d5633cd831e99c
Milan Bharanya <milan.bharanya@tundra.com>
2019-12-03 18:00:12 +0100

    Still unfinished

--- src/main/scala/Day3.scala
+++ src/main/scala/Day3.scala
@@ -6,18 +6,26 @@ object Day3 extends App {
   case class Position(x: Int, y: Int) {
     def manhattenDistanceToCenter = Math.abs((0 - x) + (0 - y))
 
-    def +(position: Position): List[Position] =
-      Math.min(x, position.x) to (Math.max(x, position.x) + x)
+    def +(move: (Int, Int, Int)): List[Position] = {
+      val (moveX, moveY, operator) = move
+
+      val list1 = for (value <- (x + operator) to (x + (moveX * operator)) by operator) yield (Position(value, y))
+      val list2 = for (value <- (y + operator) to (y + (moveY * operator)) by operator) yield (Position(x, value))
+
+      list1.toList.concat(list2.toList)
+    }
 
 
-      (Math.min(x, position.x) to (Math.max(x, position.x) + x))
-        .flatMap(newX =>
-          (Math.min(y, position.y) to (Math.max(y, position.y) + y)).map(newY => Position(newX, newY))).toList
+    //    (Math.min(x, position.x) to (Math.max(x, position.x) + x))
+    //      .flatMap(newX =>
+    //        (Math.min(y, position.y) to (Math.max(y, position.y) + y)).map(newY => Position(newX, newY))).toList
   }
+
   // Position(5, 8) + Position(0, -5)
 
 
-  sealed abstract class Command extends Product with Serializable
+  sealed abstract class Command extends Product with Serializable {
+  }
 
   object Command {
 
@@ -35,11 +43,13 @@ object Day3 extends App {
   val filename = "inputs/day_3"
   val pattern = "([R|L|U|D])([0-9]+)".r
 
+
+  val realData = Source.fromFile(filename).getLines()
   val testData = List("R8,U5,L5,D3", "U7,R6,D4,L4")
   val testData1 = List("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83")
 
 
-  val instructionsPerWire = testData.map(_.split(",")).map(instructionsForOneWire => {
+  val instructionsPerWire = testData1.map(_.split(",")).map(instructionsForOneWire => {
     instructionsForOneWire.map(instruction => {
       val pattern(direction, amount) = instruction
       direction match {
@@ -62,19 +72,29 @@ object Day3 extends App {
   }
 
 
-  val coordinatesPerWire: List[List[Position]] = instructionsPerWire
-    .map(oneWire => commandsToPos(oneWire.toList))
+  val coordinatesPerWire = instructionsPerWire
+    .map(oneWire => commandsToPos(oneWire.toList).toSet)
 
   def getPosWithPrevious(command: Command, prevPos: Position): List[Position] = {
     command match {
-      case Command.Right(amount) => prevPos + Position(0, amount)
-      case Command.Left(amount) => prevPos + Position(0, -amount)
-      case Command.Up(amount) => prevPos + Position(amount, 0)
-      case Command.Down(amount) => prevPos + Position(-amount, 0)
+      case Command.Right(amount) => prevPos + (amount, 0, 1)
+      case Command.Left(amount) => prevPos + (amount, 0, -1)
+      case Command.Up(amount) => prevPos + (0, amount, 1)
+      case Command.Down(amount) => prevPos + (0, amount, -1)
     }
   }
 
-  val flattenedCoords = coordinatesPerWire.flatten
+
+  //  val visual =
+  //    for {
+  //      x <- 0 to 10
+  //      y <- 0 to 10
+  //      char = if (coordinatesPerWire(0).contains(Position(x, y))) "#" else "."
+  //    } yield char
+  //
+  //  println(visual.grouped(10).mkString("\n"))
+
+  val flattenedCoords = coordinatesPerWire.flatten.filterNot(pos => pos.x == 0 && pos.y == 0)
   val intersections = flattenedCoords.diff(flattenedCoords.distinct).distinct
 
   val closestDistance = intersections.map(_.manhattenDistanceToCenter).min
